a098b218727740007ecd8cc86ed0ae98894f07c0
harshalwarkar2020 <harshalwarkar7@gmail.com>
2024-12-07 09:25:09 +0530

    changing code approach

--- my_app.py
+++ my_app.py
@@ -1,7 +1,7 @@
 print("Program for addition of two numbers")
 
-num1 = int(input("Enter First Number:- "))
-num2 = int(input("Enter Second Number:- "))
+num1 = 20
+num2 = 20
 
 result = num1 + num1
 print(f"addition of two numbers:- {result}")
